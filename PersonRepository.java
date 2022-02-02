0b1c22b89bae1c4923eb6f02d9dd8b0838494864
SergiuCionte <sergiuciontilici@gmail.com>
2022-02-02 23:50:01 +0200

    second commit

--- PersonRepository.java
+++ PersonRepository.java
@@ -4,5 +4,7 @@ public interface PersonRepository extends JpaRepository<Person, UUID> {
      * Example: JPA generate Query by Field
      */
     List<Person> findByName(String name);
+     
 
+     
 }
